266f3acd126a3b2f02056536f9a0f4d5feea3a6c
Crashenterminate <crashenterminatestudios@gmail.com>
2023-03-31 09:35:26 -0600

    Initial Commit

---
+++ WriteIt/WriteItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriteItApp.swift
+//  WriteIt
+//
+//  Created by Emicraft noob on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WriteItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
